537758ed0f64f67f79e6bac75e42c3db3da81989
vsilv <vsilverlord@googlemail.com>
2013-01-19 19:56:41 +0100

    put all in ocd

--- src/ocd.py
+++ src/ocd.py
@@ -3,7 +3,6 @@
 # now all the variables are stored in x
 import numpy as np
 import csv 
-import errorclass as er
 import numpy as np
 def open_csv(name,errorfy=False):
         class Bunch(object):
@@ -36,10 +35,57 @@ def open_csv(name,errorfy=False):
                 for k in variables:
                         if k[-3::]!="std":
                                 if "S"+k in errors:
-                                        result[k]=er.Errorclass(data_[k],data_["S"+k])
+                                        result[k]=Groesse(k,einheit,data_[k],data_["S"+k])
                                 else:
                                         result[k]=data_[k]
                         else:
                                 ss=np.array(data_[k]).std()
-                                result[k]=er.Errorclass(data_[k],ss)
+                                result[k]=er.Errorclass(k,einheit,data_[k],ss)
                 return Bunch(result)
+import sympy as sy
+class Groesse:
+        def __init__(self,name,einheit,x,Sx):
+                self.x=np.array(x)
+                self.Sx=np.array(Sx)
+                self.name=name
+                self.einheit=einheit
+        def __str__(self):
+                return str(self.x)+" +- "+str(self.Sx)
+        def __repr__(self):
+                return self.__str__()
+from sympy.parsing.sympy_parser import parse_expr
+import sympy as sy
+import sympy.physics.units as u
+import sympy.utilities.lambdify as lambdify
+def eval_expr(expr,variables,container):
+        vals={}
+        for k in container:
+                vals[k.name]=k.x
+                vals["S"+k.name]=k.Sx
+        #x = expr.evalf(subs=vals)
+        f=lambdify(tuple(vals.keys()),expr)
+        x = f(**vals)
+        print x
+        #little workaround
+        leer=sy.Symbol("leer")
+        f=0*leer
+
+        for k in variables:
+                f=f+ (sy.diff(expr,k))**2*sy.Symbol("S"+k.__str__())
+        f=sy.sqrt(f)
+        print f
+        #Sx= f.evalf(subs=vals)
+        gf = lambdify(tuple(vals.keys()),f)
+        Sx = gf(**vals)
+        print x,"+-",Sx
+        #units
+        unit_dict={}
+        for k in container:
+                unit_dict[k.name]=k.einheit
+                print k.name,"has unit",k.einheit
+        units=[]
+        for k in variables:
+                units+=[(k,u.__dict__[unit_dict[k.__str__()]])]
+        unit_expr=expr.subs(units)
+        print expr
+        print unit_expr
