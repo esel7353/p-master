0d2e864e0c7347985f113a96f77ad36cad7f8c85
esel7353 <esel@gegensinn.org>
2013-01-20 21:59:14 +0100

    external python files can modify markers

--- src/pm.py
+++ src/pm.py
@@ -6,41 +6,44 @@ from Environment import *
 env = Environment()
 
 def runPython(file):
-    global env
+    global env          # external python files have access to env
+    global markerTypes  # and can add more marker types
     execfile(file)
+def printPlot(name):
+    print env.getPlot(name).latex()
+def printTable(name):
+    print env.getTable(name).latex()
 
 dirlist = os.listdir('.')
 rawFiles = [rawFile for rawFile in dirlist if rawFile[-8:] == ".raw.tex"]
 
+markerTypes = {}
+markerTypes['PY'] 	= runPython
+markerTypes['PLOT']	= printPlot
+markerTypes['TABLE']	= printTable
+
+	
 
 for file in rawFiles:
     outName = file[:-8] + ".tex"
     out     = open(outName, 'w')
     sys.stdout = out
+    print >>sys.stderr, "Writing to " + outName
     for line in open(file, 'r'):
         if len(line) > 0:
             if line[0] == '#':
-                
-                # Plot marker
-                if line[:6] == '#PLOT ':
-                    name = line[6:].strip() 
-                    print env.getPlot(name).latex()
-                    continue
-                
-                # Table marker
-                if line[:7] == '#TABLE ':
-                    name = line[7:] .strip()
-                    print env.getTable(name).latex()
-                    continue
-            
-                # Python marker
-                if line[:4] == '#PY ':
-                    runPython(line[4:].strip())
-                    continue
-                
-                # unknown marker
-                print line
-                
+                if " " in line:
+                    pos    = line.index(" ") 
+                    marker = line[1:pos].strip() 
+                    arg    = line[pos:].strip()
+                else:
+                    arg    = ""
+                    marker = line[1:].strip()     
+                if marker in markerTypes:
+                    markerTypes[marker](arg)
+                else:
+                    #unknown marker
+                    print line 
             else:
                 print line
             
